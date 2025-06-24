44f77f8e0638cac01ed72f8356373dea7109500e
Ki-Tak <lee08tain@gmail.com>
2025-06-24 21:49:00 +0900

    [CHORE] AWS 및 깃허브 CI/CD 작업

--- web/src/apis/axios-instance.ts
+++ web/src/apis/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const dummyBaseURL = "http://localhost:8080/";
+const dummyBaseURL = import.meta.env.VITE_API_BASE_URL as string;
 
 export const baseAPI = axios.create({
   baseURL: dummyBaseURL,
