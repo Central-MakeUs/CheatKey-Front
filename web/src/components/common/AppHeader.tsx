780e9ce0a8f0e7606e588bc126137a9b2203fd7f
Ki-Tak <lee08tain@gmail.com>
2025-06-30 20:36:37 +0900

    feat: 헤더 임시 컴포넌트 구현

---
+++ web/src/components/common/AppHeader.tsx
@@ -0,0 +1,57 @@
+import Prev from "@/assets/icons/prev.svg?react";
+import Write from "@/assets/icons/write.svg?react";
+import Notification_On from "@/assets/icons/notification_on.svg?react";
+import Notification_Off from "@/assets/icons/notification_off.svg?react";
+interface AppHeaderProps {
+  title: string;
+  logo?: boolean;
+  onSkip?: () => void;
+  onPrev?: () => void;
+  onWrite?: () => void;
+  onNotification?: () => void;
+}
+export const AppHeader = ({
+  title,
+  logo, // 홈 화면에 로고가 아직 정해지지 않아서 우선 적용 안시켜놨습니당
+  onSkip,
+  onPrev,
+  onWrite,
+  onNotification,
+}: AppHeaderProps) => {
+  return (
+    <header className="relative w-full py-2">
+      <div className="absolute top-1/2 left-5 flex -translate-y-1/2 items-center">
+        {onPrev && (
+          <button type="button" onClick={onPrev} className="h-6 w-6">
+            <Prev className="text-base-0 h-6 w-6" />
+          </button>
+        )}
+      </div>
+      <h1 className="head-3-bold text-base-0 min-h-6 w-full text-center">
+        {title}
+      </h1>
+      <div className="absolute top-1/2 right-5 flex -translate-y-1/2 items-center gap-1">
+        {onSkip && (
+          <button
+            type="button"
+            onClick={onSkip}
+            className="caption-1-medium bg-primary-400 text-base-0 rounded-full px-3.5 py-[0.1925rem]"
+          >
+            Skip
+          </button>
+        )}
+        {onWrite && (
+          <button type="button" onClick={onWrite} className="h-8 w-8">
+            <Write className="text-base-0 h-8 w-8" />
+          </button>
+        )}
+        {onNotification && (
+          <button type="button" onClick={onNotification} className="h-8 w-8">
+            {/* 알림은 추후에 기능에 따라 컴포넌트로 변경해도 될 것으로 보입니다 */}
+            <Notification_On className="text-base-0 h-8 w-8" />
+          </button>
+        )}
+      </div>
+    </header>
+  );
+};
--- web/src/pages/auth/SignUpPage.tsx
+++ web/src/pages/auth/SignUpPage.tsx
@@ -1,7 +1,20 @@
+import { AppHeader } from "@/components/common/AppHeader";
+import Footer from "@/components/common/Footer";
+
 export const SignUpPage = () => {
+  const test = () => {
+    console.log(123);
+  };
   return (
-    <div className="relative flex h-full w-full flex-1 flex-col items-center justify-center">
-      123
+    <div className="relative flex h-full w-full flex-1 flex-col">
+      <AppHeader
+        title="커뮤니티"
+        onPrev={test}
+        //onSkip={test}
+        onNotification={test}
+        onWrite={test}
+      />
+      <Footer />
     </div>
   );
 };
