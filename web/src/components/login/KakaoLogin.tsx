965c3b4d59b485c67a84e9197de81bf719e78d5f
Ki-Tak <lee08tain@gmail.com>
2025-06-30 17:20:37 +0900

    feat: 카카오 로그인 버튼 및 버블 컴포넌트 구현

--- web/src/components/login/KakaoLogin.tsx
+++ web/src/components/login/KakaoLogin.tsx
@@ -1,3 +1,18 @@
+import kakaoLogo from "@/assets/logo/logo-kakao.svg";
+
 export const KakaoLogin = () => {
-  <button className="w-full bg-[#ffe812]"></button>;
+  const handleKakaoAuthorize = () => {};
+  return (
+    <button
+      type="button"
+      onClick={handleKakaoAuthorize}
+      aria-label="카카오로 시작하기"
+      className="flex h-13 w-full items-center justify-center gap-4 rounded-xl bg-[#FEE500] px-3 text-center align-middle"
+    >
+      <img src={kakaoLogo} className="h-5 w-5" />
+      <span className="font-sans text-base leading-5 text-[#191919]">
+        카카오로 시작하기
+      </span>
+    </button>
+  );
 };
--- web/src/pages/auth/LoginPage.tsx
+++ web/src/pages/auth/LoginPage.tsx
@@ -1,6 +1,9 @@
+import { Bubble } from "@/components/common/Bubble";
+import { KakaoLogin } from "@/components/login/KakaoLogin";
+
 export const LoginPage = () => {
   return (
-    <div className="flex h-full w-full flex-1 flex-col items-center justify-center">
+    <div className="relative flex h-full w-full flex-1 flex-col items-center justify-center">
       <div className="flex flex-col items-center gap-2">
         <div className="bg-base-0 h-40 w-40" />
         <h1 className="text-base-0 head-2-semibold mt-[1.375rem]">
@@ -9,6 +12,10 @@ export const LoginPage = () => {
         <h2 className="text-gray-system-400 body-3-regular">
           치트키에 오신 것을 환영합니다!
         </h2>
+      </div>
+      <div className="absolute bottom-11 flex w-full flex-col items-center gap-8 px-5">
+        <Bubble dir="bottom_center" text="AI로 분석하기" />
+        <KakaoLogin />
       </div>
     </div>
   );
