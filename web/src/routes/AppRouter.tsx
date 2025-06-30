ac8139f66ce6d5c36011714f03bb87c3ba59a060
tifsy <tiffanyhansy@gmail.com>
2025-06-30 21:35:52 +0900

    feat: 커뮤니티 피드, 커뮤니티 상세 보기, 글 작성 화면 라우팅

--- web/src/routes/AppRouter.tsx
+++ web/src/routes/AppRouter.tsx
@@ -1,11 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import { path } from "./path";
 import { Home } from "@/pages";
+import CommunityDetail from "@/pages/communityDetail/CommunityDetail";
+import CommunityFeed from "@/pages/communityFeed/CommunityFeed";
+import CommunityWrite from "@/pages/communityWrite/CommunityWrite";
+import BottomLayout from "@/layouts/BottomLayout";
 
 const AppRouter = createBrowserRouter([
   {
-    path: path.dummy,
-    element: <Home />,
+    element: <BottomLayout />,
+    children: [
+      {
+        path: path.home,
+        element: <Home />,
+      },
+      {
+        path: path.community.feed,
+        element: <CommunityFeed />,
+      },
+    ],
+  },
+
+  {
+    //TODO: @tifsy 커뮤니티 글 상세보기 path 변경
+    path: path.community.detail(":id"),
+    element: <CommunityDetail />,
+  },
+  {
+    path: path.community.write,
+    element: <CommunityWrite />,
   },
 ]);
 
--- web/src/routes/path.ts
+++ web/src/routes/path.ts
@@ -1,3 +1,10 @@
 export const path = {
-  dummy: "/",
+  home: "/",
+  community: {
+    base: "/community",
+    feed: "/community/feed",
+    write: "/community/write",
+    //TODO: @tifsy 커뮤니티 글 상세보기 path 변경 가능
+    detail: (id: string) => `/community/${id}`,
+  },
 };
