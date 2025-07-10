cb886f90f8996f7032f4dec57978ecd559a7eb0f
Ki-Tak <lee08tain@gmail.com>
2025-07-11 01:37:52 +0900

    fix: api 엔드포인트 변경

--- web/src/constants/apiConstants.ts
+++ web/src/constants/apiConstants.ts
@@ -1,5 +1,5 @@
 export const API_DOMAINS = {
   GET_KAKAO_LOGIN: "/api/auth/login/kakao/callback",
-  GET_AUTH_REGISTER: "/auth/register",
+  GET_AUTH_REGISTER: "/api/auth/register",
 };
 export const QUERY_KEYS = {};
