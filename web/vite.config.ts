0da9290eb5b61622dcd29133152226aaff0e5f09
Ki-Tak <lee08tain@gmail.com>
2025-07-10 22:48:04 +0900

    fix: 포트 번호 수정

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: [{ find: "@", replacement: "/src" }],
   },
   server: {
-    port: 8080,
+    port: 3000,
     host: "0.0.0.0",
     allowedHosts: true, // ngrok 허용
   },
