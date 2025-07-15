83b4d4090d2004c9e318c168ee57c9d98443a32d
Ki-Tak <lee08tain@gmail.com>
2025-07-15 15:49:26 +0900

    fix: unused 코드 삭제

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-import { Link } from "react-router-dom";
-
 import { NameTag } from "@/components/common/NameTag";
 import SearchBar from "@/components/common/SearchBar";
 import { HomeBannerList } from "@/components/home/HomeBannerList";
