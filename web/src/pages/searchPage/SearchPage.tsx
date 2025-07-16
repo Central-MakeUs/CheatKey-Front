e6deededc972752058719684c6fc405e0e93ab74
tifsy <tiffanyhansy@gmail.com>
2025-07-17 02:06:39 +0900

    feat: 검색 페이지 UI 구현

---
+++ web/src/pages/searchPage/SearchPage.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import SearchBar from "@/components/common/SearchBar";
+import { SearchResultPreview } from "@/components/searchPage/SearchResultPreview";
+
+import CuttingSad from "@/assets/images/cutting_sad.svg?react";
+
+import { mockCommunityFeedPreviews } from "@/mocks/mockCommunityFeedPreviews";
+export const SearchPage = () => {
+  const [query, setQuery] = useState("");
+
+  const filteredPosts = mockCommunityFeedPreviews.filter(
+    (post) =>
+      post.title.toLowerCase().includes(query.toLowerCase()) ||
+      post.content.toLowerCase().includes(query.toLowerCase()),
+  );
+
+  const hasNoSearchResult = query.trim() !== "" && filteredPosts.length === 0;
+
+  return (
+    <>
+      <div className="bg-bg-100 min-h-screen px-5 pt-6 pb-21">
+        <SearchBar
+          placeholder="사기 사례를 검색해주세요."
+          value={query}
+          onChange={setQuery}
+        />
+
+        {hasNoSearchResult ? (
+          <div className="flex flex-col items-center justify-center gap-5 py-18">
+            <CuttingSad className="h-[130px] w-[130px]" />
+            <p className="text-gray-system-500 body-3-regular text-center">
+              검색 결과를 찾을 수 없어요.
+            </p>
+          </div>
+        ) : (
+          <div>
+            {filteredPosts.map((post) => (
+              <SearchResultPreview
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                content={post.content}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
