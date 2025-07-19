2a6d385bce069ee73fe1b051726c3a8f7c393afb
Ki-Tak <lee08tain@gmail.com>
2025-07-20 00:22:30 +0900

    style: 로그인 페이지 로고 변경

--- web/src/pages/auth/LoginPage.tsx
+++ web/src/pages/auth/LoginPage.tsx
@@ -3,15 +3,14 @@ import { motion } from "motion/react";
 import { Bubble } from "@/components/common/Bubble";
 import { KakaoLogin } from "@/components/login/KakaoLogin";
 
+import CheatKeyTextLogo from "@/assets/logo/logo-cheatkey-text.svg?react";
+import CheatKeyLogo from "@/assets/logo/logo-cheatkey.svg?react";
+
 export const LoginPage = () => {
   return (
-    <div className="relative flex h-full w-full flex-1 flex-col items-center justify-center bg-gradient-to-b from-[rgba(11,49,255,0.3)] via-[rgba(31,32,47,0)] via-45% to-[rgba(11,49,255,0.09)]">
-      <div className="flex flex-col items-center gap-2">
-        <div className="bg-base-0 h-40 w-40" />
-        <h1 className="text-base-0 head-2-semibold mt-[1.375rem]">
-          치트키에 오신 것을 환영합니다!
-        </h1>
-      </div>
+    <div className="relative flex h-full w-full flex-1 flex-col items-center justify-center">
+      <CheatKeyLogo className="h-auto w-28" />
+      <CheatKeyTextLogo className="mt-11 h-auto w-40" />
       <div className="absolute bottom-11 flex w-full flex-col items-center gap-8 px-5">
         <motion.div
           animate={{
