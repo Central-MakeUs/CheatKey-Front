a31ac05957e46f4f0fe6ced16409a077578df7bb
Ki-Tak <lee08tain@gmail.com>
2025-07-20 02:37:52 +0900

    feat: 분석 라우팅 설정

---
+++ web/src/pages/analyze/AnalyzePage.tsx
@@ -0,0 +1,3 @@
+export const AnalyzePage = () => {
+  return <div>123</div>;
+};
--- web/src/routes/path.ts
+++ web/src/routes/path.ts
@@ -14,4 +14,7 @@ export const path = {
   search: {
     base: "/search",
   },
+  analyze: {
+    base: "/analyze",
+  },
 };
