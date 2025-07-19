5352f2c6f54f6ce99a499c4f4d2b8273b3f43174
Ki-Tak <lee08tain@gmail.com>
2025-07-20 03:10:30 +0900

    style: AppHeader 배경 색상 변경

--- web/src/pages/analyze/AnalyzePage.tsx
+++ web/src/pages/analyze/AnalyzePage.tsx
@@ -1,3 +1,9 @@
+import { AppHeader } from "@/components/common/AppHeader";
+
 export const AnalyzePage = () => {
-  return <div>123</div>;
+  return (
+    <div className="relative flex h-full w-full flex-1 flex-col items-center bg-linear-[180deg,rgba(0,40,255,0.2)_0%,rgba(34,68,109,0.1)_30%,rgba(34,68,109,0.1)_78.11%,rgba(23,40,134,0.2)_100%]">
+      <AppHeader />
+    </div>
+  );
 };
