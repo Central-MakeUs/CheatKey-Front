5fe3b9e8b17967a956ca5c809abd6f6dd764dd72
tifsy <tiffanyhansy@gmail.com>
2025-07-22 03:44:05 +0900

    design: CategoryTagGroup UI 수정

--- web/src/components/common/CategoryTagGroup.tsx
+++ web/src/components/common/CategoryTagGroup.tsx
@@ -19,7 +19,7 @@ export const CategoryTagGroup: React.FC<CategoryTagGroupProps> = ({
           type="button"
           onClick={() => onSelect(tag)}
           className={cn(
-            "body-2-medium h-9 content-center rounded-full px-3 transition-colors",
+            "body-2-medium min-width-20 h-9 content-center rounded-full px-2 transition-colors",
             selected === tag
               ? "bg-primary-400 text-base-0"
               : "bg-bg-50 text-gray-system-500",
