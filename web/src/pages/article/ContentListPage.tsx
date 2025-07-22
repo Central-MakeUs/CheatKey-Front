336531975c7cf094be2887a07c239b1a6dd76455
Ki-Tak <lee08tain@gmail.com>
2025-07-22 23:47:01 +0900

    style: 콘텐츠 애니메이션 duration 수정

--- web/src/pages/article/ContentListPage.tsx
+++ web/src/pages/article/ContentListPage.tsx
@@ -16,7 +16,6 @@ const containerVariants = {
   visible: {
     opacity: 1,
     transition: {
-      delayChildren: 0.2,
       staggerChildren: 0.1,
     },
   },
@@ -98,7 +97,11 @@ interface ContentPreviewProps {
 
 const itemVariants = {
   hidden: { y: 20, opacity: 0 },
-  visible: { y: 0, opacity: 1 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.3 },
+  },
 };
 
 const ContentPreview = ({
