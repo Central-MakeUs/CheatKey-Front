dd5f23e46ddc315077091a5bcc5b5ddbab6f2347
Ki-Tak <lee08tain@gmail.com>
2025-07-22 19:37:44 +0900

    feat: 콘텐츠 페이지 구성 및 콘텐츠 프리뷰 컴포넌트 구현

--- web/src/pages/article/ArticleListPage.tsx
+++ web/src/pages/article/ArticleListPage.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
+import { motion, AnimatePresence } from "framer-motion";
+
 import { AppHeader } from "@/components/common/AppHeader";
 import { CategoryTagGroup } from "@/components/common/CategoryTagGroup";
 
+import authorProfile from "@/assets/icons/temporary_profile_pic.png";
 export type AriticleCategory = "알려드림" | "인터뷰";
 
 export const ArticleListPage = () => {
@@ -16,8 +19,66 @@ export const ArticleListPage = () => {
         tags={["알려드림", "인터뷰"]}
         selected={articleCategory}
         onSelect={setArticleCategory}
-        className="mt-header px-5 py-2.5"
+        className="mt-header border-bg-50 border-b px-5 py-2.5"
       />
+      <AnimatePresence mode="wait">
+        <motion.h2
+          key={articleCategory}
+          className="head-3-bold text-base-0 p-5"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          {articleCategory === "알려드림"
+            ? "오늘의 사기 소식"
+            : "실제 피해 사례를 모아봤어요"}
+        </motion.h2>
+      </AnimatePresence>
+      <main className="flex h-fit w-full flex-col gap-5"></main>
     </div>
   );
+};
+
+interface ContentsPreviewProps {
+  title: string;
+  contents: string;
+  image: string;
+  date: string;
+  original: string | null;
+}
+
+const ContentsPreview = ({
+  title,
+  contents,
+  image,
+  date,
+  original,
+}: ContentsPreviewProps) => {
+  <article className="bg-bg-50 flex flex-col gap-2.5 rounded-2xl p-3">
+    <img
+      src={image}
+      alt={`${title}의 미리보기 이미지`}
+      className="h-[8.75rem] w-full rounded-xl bg-white object-fill"
+    />
+    <h3 className="body-1-bold text-gray-system-50 mb-1 w-full truncate">
+      {title}
+    </h3>
+    <p className="body-5-regular text-gray-system-400 line-clamp-2 text-ellipsis">
+      {contents}
+    </p>
+    <footer className="flex w-full justify-between">
+      <div className="flex items-center gap-2">
+        <img
+          src={authorProfile}
+          alt="콘텐츠 작성자의 프로필"
+          className="h-5 w-5"
+        />
+        <p className="body-4-medium text-primary-200">
+          {original === null ? "커팅이" : "AI Jazeera"}
+        </p>
+      </div>
+      <time className="caption-2-regular text-gray-system-600">{date}</time>
+    </footer>
+  </article>;
 };
