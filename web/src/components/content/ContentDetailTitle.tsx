1e57b1c1af6c5690d3d2eafe98614695d980b353
Ki-Tak <lee08tain@gmail.com>
2025-07-23 02:53:11 +0900

    feat: 콘텐츠 상세보기 제목 임시 컴포넌트 구현

--- web/src/components/content/ContentDetailTitle.tsx
+++ web/src/components/content/ContentDetailTitle.tsx
@@ -1,3 +1,40 @@
-export const ContentDetailTitle = () => {
-  return <div>123</div>;
+import type { ContentCategory } from "@/pages/content/ContentListPage";
+
+interface ContentDetailTileProps {
+  author: ContentCategory;
+  title: string;
+  date: string;
+  authorProfile: string;
+  className?: string;
+}
+
+export const ContentDetailTitle = ({
+  author,
+  title,
+  date,
+  authorProfile,
+  className,
+}: ContentDetailTileProps) => {
+  return (
+    <header className={`flex w-full flex-col gap-2.5 ${className}`}>
+      <div className="body-2-medium bg-bg-50 text-gray-system-500 h-9 w-fit min-w-20 content-center rounded-full px-2">
+        {author}
+      </div>
+      <h1 className="head-3-bold text-base-0 line-clamp-2">{title}</h1>
+      <div className="flex w-full items-center justify-between">
+        <div className="body-4-medium flex items-center gap-2">
+          <img
+            src={authorProfile}
+            alt="콘텐츠 작성자의 프로필"
+            className="h-5 w-5"
+          />
+          <p className="text-primary-200">
+            {author === "알려드림" && "커팅이"}
+            {author === "인터뷰" && "커팅이 리포터"}
+          </p>
+        </div>
+        <time className="caption-2-regular text-gray-system-600">{date}</time>
+      </div>
+    </header>
+  );
 };
--- web/src/pages/content/ArticleDetailPage.tsx
+++ web/src/pages/content/ArticleDetailPage.tsx
@@ -1,9 +1,31 @@
+import { useParams } from "react-router-dom";
+
 import { ContentDetailHeader } from "@/components/content/ContentDetailHeader";
+import { ContentDetailTitle } from "@/components/content/ContentDetailTitle";
+
+import authorProfile from "@/assets/icons/temporary_profile_pic.png";
+
+import { articleMap } from "@/mocks/mockContentsData";
 
 export const ArticleDetailPage = () => {
+  const { articleId } = useParams();
+
+  const data = articleId ? articleMap.get(parseInt(articleId, 10)) : undefined;
+
+  if (!data) {
+    return <div>게시물을 찾을 수 없습니다.</div>;
+  }
+
   return (
     <div className="bg-bg-100 flex min-h-screen flex-col">
       <ContentDetailHeader />
+      <ContentDetailTitle
+        className="mt-header p-5"
+        author="알려드림"
+        title={data.title}
+        date={data.date}
+        authorProfile={authorProfile}
+      />
     </div>
   );
 };
