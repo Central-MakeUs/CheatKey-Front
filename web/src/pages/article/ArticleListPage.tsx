1220323c4b7b5ddfee68d084e4fbf77a95b89bb0
Ki-Tak <lee08tain@gmail.com>
2025-07-22 15:06:46 +0900

    refactor: 카테고리 태그 그룹 컴포넌트 제네릭 타입 확장 및 애니메이션 추가

--- web/src/pages/article/ArticleListPage.tsx
+++ web/src/pages/article/ArticleListPage.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
+import { AppHeader } from "@/components/common/AppHeader";
+import { CategoryTagGroup } from "@/components/common/CategoryTagGroup";
+
+export type AriticleCategory = "알려드림" | "인터뷰";
+
 export const ArticleListPage = () => {
-  return <div></div>;
+  const [articleCategory, setArticleCategory] =
+    useState<AriticleCategory>("알려드림");
+  return (
+    <div className="bg-bg-100 min-h-screen pb-21">
+      {/** TODO: @Ki-Tak 추후에 알림 버튼 함수 수정해야함 */}
+      <AppHeader title="콘텐츠" onNotification={() => console.log("알림")} />
+      <CategoryTagGroup
+        tags={["알려드림", "인터뷰"]}
+        selected={articleCategory}
+        onSelect={setArticleCategory}
+        className="mt-header px-5 py-2.5"
+      />
+    </div>
+  );
 };
