337a2809a9426878d2acf97c3a3d6bd4e4155ad0
Ki-Tak <lee08tain@gmail.com>
2025-07-22 20:21:03 +0900

    feat: 콘텐츠 리스트 뷰 등장 애니메이션 구현

--- web/src/pages/article/ContentListPage.tsx
+++ web/src/pages/article/ContentListPage.tsx
@@ -8,13 +8,25 @@ import { CategoryTagGroup } from "@/components/common/CategoryTagGroup";
 import authorProfile from "@/assets/icons/temporary_profile_pic.png";
 
 import { articleData, interviewData } from "@/mocks/mockContentsData";
+
 export type ContentCategory = "알려드림" | "인터뷰";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export const ContentListPage = () => {
   const [articleCategory, setArticleCategory] =
     useState<ContentCategory>("알려드림");
   return (
-    <div className="bg-bg-100 min-h-screen pb-21">
+    <div className="bg-bg-100 min-h-screen pb-29">
       {/** TODO: @Ki-Tak 추후에 알림 버튼 함수 수정해야함 */}
       <AppHeader title="콘텐츠" onNotification={() => console.log("알림")} />
       <CategoryTagGroup
@@ -37,29 +49,40 @@ export const ContentListPage = () => {
             : "실제 피해 사례를 모아봤어요"}
         </motion.h2>
       </AnimatePresence>
-      <main className="flex h-fit w-full flex-col gap-5 px-5">
-        {articleCategory === "알려드림" &&
-          articleData.map((data) => (
-            <ContentPreview
-              key={data.title}
-              title={data.title}
-              image={data.image}
-              date={data.date}
-              original={data.original}
-              contents={data.sections[0].contents as string}
-            />
-          ))}
-        {articleCategory === "인터뷰" &&
-          interviewData.map((data) => (
-            <ContentPreview
-              key={data.title}
-              title={data.title}
-              image={data.image}
-              date={data.date}
-              original={data.original}
-              contents={data.sections[0].contents as string}
-            />
-          ))}
+      <main>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={articleCategory}
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit={{ opacity: 0 }}
+            className="flex h-fit w-full flex-col gap-5 px-5"
+          >
+            {articleCategory === "알려드림" &&
+              articleData.map((data) => (
+                <ContentPreview
+                  key={data.title}
+                  title={data.title}
+                  image={data.image}
+                  date={data.date}
+                  original={data.original}
+                  contents={data.sections[0].contents as string}
+                />
+              ))}
+            {articleCategory === "인터뷰" &&
+              interviewData.map((data) => (
+                <ContentPreview
+                  key={data.title}
+                  title={data.title}
+                  image={data.image}
+                  date={data.date}
+                  original={data.original}
+                  contents={data.sections[0].contents as string}
+                />
+              ))}
+          </motion.div>
+        </AnimatePresence>
       </main>
     </div>
   );
@@ -73,6 +96,11 @@ interface ContentPreviewProps {
   original: string | null;
 }
 
+const itemVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+};
+
 const ContentPreview = ({
   title,
   contents,
@@ -80,7 +108,10 @@ const ContentPreview = ({
   date,
   original,
 }: ContentPreviewProps) => (
-  <article className="bg-bg-50 flex flex-col gap-2.5 rounded-2xl p-3">
+  <motion.article
+    variants={itemVariants}
+    className="bg-bg-50 flex flex-col gap-2.5 rounded-2xl p-3"
+  >
     <img
       src={image}
       alt={`${title}의 미리보기 이미지`}
@@ -92,18 +123,23 @@ const ContentPreview = ({
     <p className="body-5-regular text-gray-system-400 line-clamp-2 text-ellipsis">
       {contents}
     </p>
-    <footer className="flex w-full justify-between">
-      <div className="flex items-center gap-2">
+    <footer className="flex w-full items-center justify-between">
+      <div className="body-4-medium flex items-center gap-2">
         <img
           src={authorProfile}
           alt="콘텐츠 작성자의 프로필"
           className="h-5 w-5"
         />
-        <p className="body-4-medium text-primary-200">
-          {original === null ? "커팅이" : "AI Jazeera"}
-        </p>
+
+        <p className="text-primary-200">커팅이</p>
+        {original !== null && (
+          <>
+            <div className="bg-gray-system-700 h-5 w-[0.5px]" />
+            <span className="text-gray-system-600">참고자료</span>
+          </>
+        )}
       </div>
       <time className="caption-2-regular text-gray-system-600">{date}</time>
     </footer>
-  </article>
+  </motion.article>
 );
