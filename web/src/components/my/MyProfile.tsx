d4f7fc84aecd109699ddb4d66427cc3c561347b9
tifsy <tiffanyhansy@gmail.com>
2025-07-27 01:18:56 +0900

    feat: 프로필 수정 경로 연결

--- web/src/components/my/MyProfile.tsx
+++ web/src/components/my/MyProfile.tsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+import { path } from "@/routes/path";
+
 import { NameTag } from "@/components/common/NameTag";
 
 import ArrowRightIcon from "@/assets/icons/arrow_right.svg?react";
 import TemporaryProfileImage from "@/assets/icons/temporary_profile_pic.svg";
 
 export const MyProfile = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="mt-[4rem] mb-[1.875rem] flex items-center gap-[27px]">
       <img
@@ -15,7 +21,10 @@ export const MyProfile = () => {
         <NameTag type="home" name="LV.1 탐지 훈련" />
         <div className="mt-3 flex items-center gap-2">
           <h1 className="head-3-bold text-base-0 truncate">닉네임5자</h1>
-          <button aria-label="프로필 수정하기">
+          <button
+            aria-label="프로필 수정하기"
+            onClick={() => navigate(path.my.edit)}
+          >
             <ArrowRightIcon />
           </button>
         </div>
