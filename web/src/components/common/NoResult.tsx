6577ff385e35379ce557ec5736335e8d3c568f7b
tifsy <tiffanyhansy@gmail.com>
2025-07-27 04:35:02 +0900

    feat: 결과가 없을 때 보여지는 NoResult 컴포넌트 구현

---
+++ web/src/components/common/NoResult.tsx
@@ -0,0 +1,55 @@
+import { useNavigate } from "react-router-dom";
+
+import { path } from "@/routes/path";
+
+import AnalysisStarIcon from "@/assets/icons/analysis_star.svg?react";
+import WriteIcon from "@/assets/icons/write_off.svg?react";
+import CuttingSad from "@/assets/images/cutting_sad.svg?react";
+
+interface NoResultProps {
+  text: string;
+  type: "ai" | "write" | "none";
+}
+
+export const NoResult = ({ text, type = "none" }: NoResultProps) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (type === "write") {
+      navigate(path.community.write);
+    } else if (type === "ai") {
+      navigate(path.analyze.base);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 py-18">
+      <CuttingSad
+        className="h-[130px] w-[130px]"
+        aria-label="검색 결과가 없어서 슬픈 커팅이"
+      />
+      <p className="text-gray-system-500 body-3-regular text-center break-words whitespace-pre-line">
+        {text}
+      </p>
+      {type !== "none" && (
+        <button
+          className="bg-bg-50 body-1-bold text-gray-system-500 mt-2.5 flex h-[42px] w-fit items-center justify-center gap-[1px] rounded-full px-3"
+          onClick={handleClick}
+          aria-label={type === "write" ? "글 작성하기" : "AI로 분석하기"}
+        >
+          {type === "write" ? (
+            <>
+              <span>글 작성하기</span>
+              <WriteIcon aria-hidden="true" focusable="false" />
+            </>
+          ) : (
+            <>
+              <span>AI로 분석하기</span>
+              <AnalysisStarIcon aria-hidden="true" focusable="false" />
+            </>
+          )}
+        </button>
+      )}
+    </div>
+  );
+};
