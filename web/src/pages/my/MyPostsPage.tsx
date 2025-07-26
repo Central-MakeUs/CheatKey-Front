497afb340ed5aa6350b17b12f8a7bb811c0002de
tifsy <tiffanyhansy@gmail.com>
2025-07-27 04:39:39 +0900

    feat: 작성글 보기 페이지에 내 작성글이 없을 경우 예외처리

--- web/src/pages/my/MyPostsPage.tsx
+++ web/src/pages/my/MyPostsPage.tsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 
 import { AppHeader } from "@/components/common/AppHeader";
+import { NoResult } from "@/components/common/NoResult";
 import { ToTop } from "@/components/common/ToTop";
 import { MyPostsPreview } from "@/components/my/MyPostsPreview";
 
 import { mockMyPosts } from "@/mocks/mockMyPosts";
+
 export const MyPostsPage = () => {
   const navigate = useNavigate();
 
@@ -17,19 +19,24 @@ export const MyPostsPage = () => {
         <span className="text-base-0 head-4-semibold w-fit py-[0.75rem]">
           총 2개의 작성글
         </span>
-        <div className="divide-bg-50 divide-y">
-          {posts.map((post) => (
-            <MyPostsPreview
-              nickname={post.nickname}
-              id={post.id}
-              date={post.date}
-              title={post.title}
-              content={post.content}
-              commentCount={post.commentCount}
-              images={post.images}
-            />
-          ))}
-        </div>
+        {posts.length === 0 ? (
+          <NoResult text="작성한 글이 없습니다." type="none" />
+        ) : (
+          <div className="divide-bg-50 divide-y">
+            {posts.map((post) => (
+              <MyPostsPreview
+                key={post.id}
+                nickname={post.nickname}
+                id={post.id}
+                date={post.date}
+                title={post.title}
+                content={post.content}
+                commentCount={post.commentCount}
+                images={post.images}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <ToTop bottom="2rem" />
     </div>
