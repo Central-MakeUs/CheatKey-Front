a23e075ced4ff1c5ddc893c3ff6150ce56bfbb3e
tifsy <tiffanyhansy@gmail.com>
2025-07-28 05:16:38 +0900

    feat: 내 분석 내역 보기 페이지 UI 구현

---
+++ web/src/constants/myAnalysisPageTabs.ts
@@ -0,0 +1,3 @@
+//리뷰
+
+export const MY_ANALYSIS_PAGE_TABS = ["오늘", "일주일", "3개월"];
--- web/src/pages/my/MyAnalysisPage.tsx
+++ web/src/pages/my/MyAnalysisPage.tsx
@@ -1,3 +1,32 @@
+//리뷰
+
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { AppHeader } from "@/components/common/AppHeader";
+import { ToTop } from "@/components/common/ToTop";
+import { MyAnalysisList } from "@/components/my/MyAnalysisList";
+import { MyAnalysisPageTab } from "@/components/my/MyAnalysisPageTab";
+
+import { MY_ANALYSIS_PAGE_TABS } from "@/constants/myAnalysisPageTabs";
+
+import { mockMyAnalysisPageData } from "@/mocks/mockMyAnalysisPageData";
+
 export const MyAnalysisPage = () => {
-  return <div>MyAnalysisPage</div>;
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState<string>(MY_ANALYSIS_PAGE_TABS[0]);
+
+  return (
+    <div className="bg-bg-100">
+      <AppHeader onPrev={() => navigate(-1)} title="분석 내역 보기" />
+
+      <div className="divide-bg-50 divide-y px-5 pt-11">
+        <MyAnalysisPageTab activeTab={activeTab} setActiveTab={setActiveTab} />
+        <MyAnalysisList items={mockMyAnalysisPageData} />
+      </div>
+
+      <ToTop bottom="2rem" />
+    </div>
+  );
 };
