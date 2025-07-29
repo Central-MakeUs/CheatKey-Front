1cea35dd3c0b7a7347943c90516403b5b06f8dab
tifsy <tiffanyhansy@gmail.com>
2025-07-30 03:17:20 +0900

    refactor: 이용약관 페이지 h2 태그로 시맨틱 마크업 수정

--- web/src/components/my/TermItem.tsx
+++ web/src/components/my/TermItem.tsx
@@ -11,9 +11,9 @@ export const TermItem = ({ title, content }: TermItemProps) => {
       aria-labelledby="term-bottomsheet-title"
       className="flex flex-col gap-5 overflow-y-auto px-5 py-[1.5625rem]"
     >
-      <h1 id="term-bottomsheet-title" className="head-2-semibold text-base-0">
+      <h2 id="term-bottomsheet-title" className="head-2-semibold text-base-0">
         {title}
-      </h1>
+      </h2>
       <p className="body-5-regular text-gray-system-300 whitespace-pre-line">
         {content}
       </p>
