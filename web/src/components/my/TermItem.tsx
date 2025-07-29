5e85ed957adfd66de4c11ae78d8b04d8af27f8de
tifsy <tiffanyhansy@gmail.com>
2025-07-30 03:19:03 +0900

    refactor: 이용약관 aria-labelledby 및 id 수정

--- web/src/components/my/TermItem.tsx
+++ web/src/components/my/TermItem.tsx
@@ -8,10 +8,10 @@ interface TermItemProps {
 export const TermItem = ({ title, content }: TermItemProps) => {
   return (
     <div
-      aria-labelledby="term-bottomsheet-title"
+      aria-labelledby="term"
       className="flex flex-col gap-5 overflow-y-auto px-5 py-[1.5625rem]"
     >
-      <h2 id="term-bottomsheet-title" className="head-2-semibold text-base-0">
+      <h2 id="term-title" className="head-2-semibold text-base-0">
         {title}
       </h2>
       <p className="body-5-regular text-gray-system-300 whitespace-pre-line">
