a7eee4b22e089474efb198dfa34c69d102371d41
tifsy <tiffanyhansy@gmail.com>
2025-07-31 01:15:31 +0900

    design: 마이페이지 아이콘 색 수정

--- web/src/components/my/MyProfile.tsx
+++ web/src/components/my/MyProfile.tsx
@@ -25,7 +25,7 @@ export const MyProfile = () => {
             aria-label="프로필 수정하기"
             onClick={() => navigate(path.my.edit)}
           >
-            <ArrowRightIcon />
+            <ArrowRightIcon className="text-gray-system-700" />
           </button>
         </div>
 
