28280a55f70a937ca03bc71e04c9c41ac55fc29f
tifsy <tiffanyhansy@gmail.com>
2025-08-04 00:57:36 +0900

    refactor: 커뮤니티 상세보기 사진 확대 모달을 Fragment 외부로 분리하여 구조 개선

--- web/src/components/communityDetail/CommunityPostContent.tsx
+++ web/src/components/communityDetail/CommunityPostContent.tsx
@@ -22,62 +22,68 @@ export const CommunityPostContent = ({
 }: CommunityPostContentProps) => {
   const { selectedImage, openImageCloseUp, closeImageCloseUp } =
     useImageCloseUp();
+
   return (
-    <div className="space-y-2 px-5 py-5">
-      <div className="flex items-start justify-between">
-        <div className="flex gap-3">
-          {/* TODO: @tifsy 사용자 프로필 사진 불러오기 */}
-          <img
-            src={TemporaryProfilePicIcon}
-            alt={`${nickname}님의 프로필 이미지`}
-            className="h-15 w-15 rounded-[100px]"
-          />
-          <div className="flex flex-col gap-y-[10px]">
-            <div className="bg-bg-50 text-gray-system-500 body-4-medium h-7 w-fit content-center rounded-lg px-[10px] py-1">
-              {nickname}
+    <>
+      <div className="space-y-2 px-5 py-5">
+        <div className="flex items-start justify-between">
+          <div className="flex gap-3">
+            {/* TODO: @tifsy 사용자 프로필 사진 불러오기 */}
+            <img
+              src={TemporaryProfilePicIcon}
+              alt={`${nickname}님의 프로필 이미지`}
+              className="h-15 w-15 rounded-full"
+            />
+            <div className="flex flex-col gap-y-2.5">
+              <div className="bg-bg-50 text-gray-system-500 body-4-medium h-7 w-fit content-center rounded-lg px-2.5 py-1">
+                {nickname}
+              </div>
+              <span className="text-gray-system-600 body-5-regular">
+                {date}
+              </span>
             </div>
-            <span className="text-gray-system-600 body-5-regular">{date}</span>
           </div>
+          <PostMenuButton />
         </div>
-        <PostMenuButton />
+
+        <div className="text-gray-system-100 body-1-bold">{title}</div>
+
+        <p className="text-gray-system-400 body-5-regular line-clamp-3">
+          {content}
+        </p>
+
+        {images.length > 0 && (
+          <div
+            className={cn(
+              "flex gap-[0.4375rem]",
+              images.length >= 3 && "scrollbar-hide overflow-x-auto",
+            )}
+          >
+            {images
+              .slice(0, images.length >= 3 ? images.length : 2)
+              .map((img, i) => (
+                <img
+                  key={`${img}-${i}`}
+                  src={img}
+                  alt={`${nickname}님의 게시글 이미지 ${i + 1}`}
+                  className={cn("h-[6.875rem] rounded-lg object-cover", {
+                    "w-full": images.length === 1,
+                    "w-1/2": images.length === 2,
+                    "w-[8.125rem] flex-shrink-0": images.length >= 3,
+                  })}
+                  onClick={() => openImageCloseUp(img)}
+                />
+              ))}
+          </div>
+        )}
       </div>
 
-      <div className="text-gray-system-100 body-1-bold">{title}</div>
-
-      <p className="text-gray-system-400 body-5-regular line-clamp-3">
-        {content}
-      </p>
-
-      {images.length > 0 && (
-        <div
-          className={cn(
-            "flex gap-[0.4375rem]",
-            images.length >= 3 && "scrollbar-hide overflow-x-auto",
-          )}
-        >
-          {images
-            .slice(0, images.length >= 3 ? images.length : 2)
-            .map((img, i) => (
-              <img
-                key={`${img}-${i}`}
-                src={img}
-                alt={`${nickname}님의 게시글 이미지 ${i + 1}`}
-                className={cn("h-[6.875rem] rounded-lg object-cover", {
-                  "w-full": images.length === 1,
-                  "w-1/2": images.length === 2,
-                  "w-[8.125rem] flex-shrink-0": images.length >= 3,
-                })}
-                onClick={() => openImageCloseUp(img)}
-              />
-            ))}
-          {selectedImage && (
-            <ImageCloseUpModal
-              imageUrl={selectedImage}
-              onClose={closeImageCloseUp}
-            />
-          )}
-        </div>
+      {selectedImage && (
+        <ImageCloseUpModal
+          imageUrl={selectedImage}
+          onClose={closeImageCloseUp}
+        />
       )}
-    </div>
+    </>
   );
 };
