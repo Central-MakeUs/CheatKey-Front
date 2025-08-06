99b5a462911988805be336a78e4cd890e19b759a
Ki-Tak <lee08tain@gmail.com>
2025-08-07 02:15:39 +0900

    feat: 온보딩 데이터 상수 구현

---
+++ web/src/constants/onboardingConstants.ts
@@ -0,0 +1,26 @@
+import onboardImg1 from "@/assets/onboarding/onboarding_1.svg";
+import onboardImg2 from "@/assets/onboarding/onboarding_2.svg";
+import onboardImg3 from "@/assets/onboarding/onboarding_3.svg";
+import onboardImg4 from "@/assets/onboarding/onboarding_4.svg";
+export const ONBOARDING_CONSTANTS = {
+  1: {
+    title: "URL 분석하기",
+    subTitle: `의심되는 링크를 복사하고\n사전에 미리 방지할 수 있어요!`,
+    image: onboardImg1,
+  },
+  2: {
+    title: "텍스트 분석하기",
+    subTitle: `의심되는 상대방의 텍스트를 분석하고\n사기꾼보다 발빠르게 움직여요.`,
+    image: onboardImg2,
+  },
+  3: {
+    title: "사기 사례 공유하기",
+    subTitle: `다양한 사기 사례를 커뮤니티에서\n함께 공유할 수 있어요.`,
+    image: onboardImg3,
+  },
+  4: {
+    title: "콘텐츠",
+    subTitle: `최근 발생하는 사기 뉴스에 대한\n소식을 가장 빠르게 접할수 있어요.`,
+    image: onboardImg4,
+  },
+};
--- web/src/pages/onboarding/OnboardingPage.tsx
+++ web/src/pages/onboarding/OnboardingPage.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
+import { BottomFullButton } from "@/components/common/BottomFullButton";
+import { PageIndicator } from "@/components/common/PageIndicator";
+
 export const OnboardingPage = () => {
-  return <div></div>;
+  const [stepState, setStepState] = useState<number>(1);
+  return (
+    <div className="safearea bg-bg-100 relative flex h-screen w-full flex-1 flex-col justify-center">
+      <div className="flex w-full flex-1 bg-amber-300">
+        <h1 className="head-2-semibold text-base-0">URL 분석하기</h1>
+        <h2 className="body-2-medium text-gray-system-400"></h2>
+      </div>
+      <PageIndicator
+        total={4}
+        current={stepState}
+        indicatorColor="bg-primary-400"
+      />
+      <div className="border-t-gray-system-800 w-full border-t px-5 py-3">
+        <BottomFullButton
+          content={"시작하기"}
+          state={stepState === 4}
+          onClick={() => setStepState((prev) => prev + 1)}
+        />
+      </div>
+    </div>
+  );
 };
