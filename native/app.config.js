7fde851a4b2583ca0bdde891204b5236bd22a027
Ki-Tak <lee08tain@gmail.com>
2025-08-07 00:50:28 +0900

    chore: 스플래쉬 화면 구현

--- native/app.config.js
+++ native/app.config.js
@@ -47,10 +47,10 @@ module.exports = () => {
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
-          imageWidth: 200,
+          image: "./assets/images/cheatkey.png",
+          imageWidth: 180,
           resizeMode: "contain",
-          backgroundColor: "#ffffff",
+          backgroundColor: "#161517",
         },
       ],
       [
