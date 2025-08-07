0bb613b13b61ca9ed21fbf217b75abc2f7a18a70
tifsy <tiffanyhansy@gmail.com>
2025-08-07 23:08:09 +0900

    feat: 마이페이지 api 연동

---
+++ web/src/apis/my/getMypageDashboard.ts
@@ -0,0 +1,10 @@
+import { authAPI } from "@/apis/instance";
+import type { MypageDashboardResponse } from "@/types/my/my.types";
+
+import { API_DOMAINS } from "@/constants/apiConstants";
+
+export const getMypageDashboard =
+  async (): Promise<MypageDashboardResponse> => {
+    const response = await authAPI.get(API_DOMAINS.GET_MYPAGE_DASHBOARD);
+    return response.data;
+  };
