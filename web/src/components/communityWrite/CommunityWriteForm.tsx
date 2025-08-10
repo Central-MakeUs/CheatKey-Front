10a8d0dbd7ec1be181757dd62e1b4623967527f0
Ki-Tak <lee08tain@gmail.com>
2025-08-11 03:00:06 +0900

    feat: 커뮤니티 이용 규칙 바텀 시트 및 상수 추가

--- web/src/components/communityWrite/CommunityWriteForm.tsx
+++ web/src/components/communityWrite/CommunityWriteForm.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 import { FormTextarea } from "@/components/common/FormTextarea";
+import { TermBottomSheet } from "@/components/signup/TermBottomSheet";
+
+import { WRITE_RULE } from "@/constants/writeRule";
+
+import Rule from "@/assets/icons/alert.svg?react";
 
 export const CommunityWriteForm = ({
   value,
@@ -7,9 +14,18 @@ export const CommunityWriteForm = ({
   value: string;
   onChange: (value: string) => void;
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div>
-      <h1 className="head-4-semibold text-base-0 pb-3">
+      <h1 className="head-4-semibold text-base-0 flex gap-1 pb-3">
+        <button
+          type="button"
+          aria-label="커뮤니티 이용규칙 보기"
+          onClick={() => setIsOpen(true)}
+          className="h-6 w-6"
+        >
+          <Rule className="h-full w-full" />
+        </button>
         내용을 입력해주세요.<span className="text-primary-400">*</span>
       </h1>
       <FormTextarea
@@ -19,6 +35,12 @@ export const CommunityWriteForm = ({
         onChange={onChange}
         maxLength={500}
         placeholder="최소 10자 이상 작성해주세요."
+      />
+      <TermBottomSheet
+        title={WRITE_RULE.title}
+        content={WRITE_RULE.content}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
       />
     </div>
   );
