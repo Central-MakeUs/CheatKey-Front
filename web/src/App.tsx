d30bfc7919e5746bd758695d5a19b88f4467c51d
Ki-Tak <lee08tain@gmail.com>
2025-08-26 20:44:30 +0900

    chore: 리액트 토스트 패키지 제거

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { toast } from "react-toastify";
 
 import AppRouter from "@/routes/AppRouter";
 
@@ -12,9 +11,7 @@ const queryClient = new QueryClient({
       throwOnError: false,
     },
     mutations: {
-      onError: (error) => {
-        toast.error(error.message);
-      },
+      throwOnError: false,
     },
   },
 });
