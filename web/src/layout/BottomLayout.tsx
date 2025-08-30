1a8f053c4602b9642698285435849a031e71d0b8
Ki-Tak <lee08tain@gmail.com>
2025-08-30 11:53:26 +0900

    refactor: 이전 라우팅 관련 리팩토링으로  바텀 네비바 불필요한 변수 삭제

--- web/src/layout/BottomLayout.tsx
+++ web/src/layout/BottomLayout.tsx
@@ -1,26 +1,12 @@
-import { useLocation, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import { BottomNavBar } from "@/components/common/BottomNavBar";
 
-import { PAGE_PATH } from "@/constants/path";
-
-const HIDDEN_PATHS = [
-  PAGE_PATH.COMMUNITY.SPECIFIC.WRITE,
-  /^\/community\/\d+$/,
-  /^\/content\/(?:article|interview)\/[^/]+$/,
-];
-
 export const BottomLayout = () => {
-  const { pathname } = useLocation();
-
-  const hideBottomNavBar = HIDDEN_PATHS.some((p) =>
-    typeof p === "string" ? p === pathname : p.test(pathname),
-  );
-
   return (
     <>
       <Outlet />
-      {!hideBottomNavBar && <BottomNavBar />}
+      <BottomNavBar />
     </>
   );
 };
