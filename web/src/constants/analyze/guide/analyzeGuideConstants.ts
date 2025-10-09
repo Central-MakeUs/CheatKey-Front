f07499569e74e2da90e22284be8de4f0ec2f63f7
Ki-Tak <lee08tain@gmail.com>
2025-10-10 01:52:39 +0900

    feat: 가이드 메인 섹션 구현

--- web/src/constants/analyze/guide/analyzeGuideConstants.ts
+++ web/src/constants/analyze/guide/analyzeGuideConstants.ts
@@ -1,30 +1,47 @@
+import guideStep1Img from "@/assets/guide/analyze_guide_1.png";
+import guideStep2Img from "@/assets/guide/analyze_guide_2.png";
+import guideStep3Img from "@/assets/guide/analyze_guide_3.png";
+import guideStep4Img from "@/assets/guide/analyze_guide_4.png";
+
 export const ANALYZE_GUIDE_EXPLAIN = {
-  MARK: "처음이시라면?",
-  UNDERLINE: "10초만에",
-  TITLE: "분석하는",
-  STRONG_TITLE: "AI 이용 가이드",
-  SUB_TITLE:
+  mark: "처음이시라면?",
+  underline: "10초만에",
+  title: "분석하는",
+  strongTitle: "AI 이용 가이드",
+  subTitle:
     "의심 가는 사이트, 거래 글, 수상한 행동들\n모두 입력하여 분석하기 버튼만 눌러주면 끝!",
 };
+
 export const ANALYZE_GUIDE_CONTENTS = [
   {
-    IMAGE: "",
-    TITLE: "",
-    CONTENT: "",
+    image: guideStep1Img,
+    title: "의심되는 사이트나 내용 복사,붙여넣기",
+    content:
+      "의심가는 사이트 주소를 받거나 거래 글이 있다면 복사해서 붙여만 넣어주세요! AI가 10초만에 위험도를 판독합니다.",
   },
   {
-    IMAGE: "",
-    TITLE: "",
-    CONTENT: "",
+    image: guideStep2Img,
+    title: "의심되는 내용이 있다면?",
+    content:
+      "'갑자기 이상한 톡방에 초대됐어요..' 등 정말 뜬금없는 수상한 상황이라도 사례 분석에 입력해서 분석하기를 눌러주세요! 해당 내용들도 똑같이 위험도를 판독합니다.",
   },
   {
-    IMAGE: "",
-    TITLE: "",
-    CONTENT: "",
+    image: guideStep3Img,
+    title: "직접 이야기 나누기",
+    content:
+      "실제 사람들에게 직접 물어보거나 이야기를 함께 나눌 수도 있어요. 커뮤니티를 이용하여 다양한 사람들과 사기에 대해 이야기 해보세요!",
   },
   {
-    IMAGE: "",
-    TITLE: "",
-    CONTENT: "",
+    image: guideStep4Img,
+    title: "사기 소식 빠르게 확인하기",
+    content:
+      "여러 사기 소식과 뉴스를 재빠르게 확인해 보세요. 사기 소식과 뉴스 등 빠르게 업로드 되는 내용들을 확인하여 발 빠르게 대처할 수 있어요.",
   },
 ];
+
+export const ANALYZE_GUIDE_FOOTER = {
+  heading: "앱 사용 시 문의사항이 있다면",
+  email: "cheattkey@gmail.com",
+  emailText: "로 연락 주세요.",
+  response: "24시간 이내에 회신 드립니다.",
+};
